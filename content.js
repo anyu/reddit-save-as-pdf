5cbe3d1f88eb2391ce4618df00bc6021e2b1e004
An Yu <1009227+anyu@users.noreply.github.com>
2023-09-19 14:45:35 -0400

    light cleanup

--- content.js
+++ content.js
@@ -1,53 +1,74 @@
-const COMMENT_SELECTOR = 'shreddit-comment-action-row[slot="actionRow"]';
-const COMMENT_SHARE_SLOT_SELECTOR = 'slot[name="comment-share"]';
-const COMMENT_SAVE_AS_PDF_SLOT_SELECTOR = 'slot[name="comment-save-as-pdf"]';
+// Expected DOM structure
+/* <shreddit-comment-action-row...slot="actionRow">
+    <#shadow-root> (open)
+      <div>
+        ...
+        <slot name="comment-reply"></slot>
+        <slot name="comment-share"></slot>
+        <slot name="comment-save-as-pdf"></slot>
+      </div>
+    <faceplate-tracker...slot=comment-reply>
 
-const COMMENT_CONTENT_SELECTOR = '#-post-rtjson-content';
-const COMMENT_SHARE_BUTTON_SELECTOR = 'shreddit-comment-share-button';
-const COMMENT_SHARE_BUTTON_SPAN_SELECTOR = 'button span:first-child span:last-child';
+    <shreddit-comment-share-button...slot-comment-share>
+      <#shadow-root> (open)
+      <button>
+        <span>
+          <span></span>
+          <span></span>
+          <span>Share</span>
+        </span
+      </button>
+    </shreddit-comment-share-button>
 
-function addSaveAsPdfButton(shadowHost) {
-  // Find the <slot> element within the Shadow DOM
+    <button id="saveAsPdfButton" slot=comment-save-as-pdf"></button>
+   </shreddit-comment-action-row>
+*/
 
-  const commentShareSlot = shadowHost.shadowRoot.querySelector(COMMENT_SHARE_SLOT_SELECTOR);
+const COMMENT_ROW = 'shreddit-comment-action-row[slot="actionRow"]';
+const COMMENT_SHARE_SLOT = 'slot[name="comment-share"]';
+const COMMENT_SAVE_AS_PDF_SLOT = 'slot[name="comment-save-as-pdf"]';
 
+const COMMENT_SHARE_BUTTON = 'shreddit-comment-share-button';
+const COMMENT_SHARE_BUTTON_SPAN = 'button span:first-child span:last-child';
+
+const COMMENT_TEXT = '#-post-rtjson-content';
+
+function addSaveAsPdfButton(rootComment) {
+  const commentShareSlot = rootComment.shadowRoot.querySelector(COMMENT_SHARE_SLOT);
   if (commentShareSlot) {
     const parentElement = commentShareSlot.parentElement;
+    const saveAsPDFButton = parentElement.querySelector(COMMENT_SAVE_AS_PDF_SLOT);
 
-    const saveAsPDFButton = parentElement.querySelector(COMMENT_SAVE_AS_PDF_SLOT_SELECTOR);
-
-    // only if Save as PDF button not already added
+    // If Save as PDF button doesn't already exist
     if (!saveAsPDFButton) {
-      // create and insert <slot name="comment-save-as-pdf"></slot> after share slot
+      // Create and insert <slot name="comment-save-as-pdf"></slot> after share slot
       const commentSaveAsPdfSlot = document.createElement('slot');
       commentSaveAsPdfSlot.setAttribute('name', 'comment-save-as-pdf');
-  
       parentElement.insertBefore(commentSaveAsPdfSlot, commentShareSlot.nextSibling.nextSibling);
   
-      const shareButton = shadowHost.querySelector(
-        COMMENT_SHARE_BUTTON_SELECTOR
-      );
-      
+      const shareButton = rootComment.querySelector(COMMENT_SHARE_BUTTON);
       if (shareButton) {  
-        // create and insert save as pdf button
 
-        const shareButtonSpan = shareButton.shadowRoot.querySelector(COMMENT_SHARE_BUTTON_SPAN_SELECTOR)
-
+        // Create new button
         const saveAsPdfButton = document.createElement("button");
         saveAsPdfButton.textContent = "Save as PDF";
         saveAsPdfButton.id = "saveAsPdfButton";
         saveAsPdfButton.slot = "comment-save-as-pdf"
   
+        // Match styles by using share button and span styles
         const shareButtonStyles = getComputedStyle(shareButton);
-
         saveAsPdfButton.style.backgroundColor = shareButtonStyles.backgroundColor;
-        saveAsPdfButton.style.paddingLeft = "10px"
-        saveAsPdfButton.style.paddingRight = "10px"
 
         // Some styles are on the nested span
+        const shareButtonSpan = shareButton.shadowRoot.querySelector(COMMENT_SHARE_BUTTON_SPAN)
         const shareButtonSpanStyles = getComputedStyle(shareButtonSpan);
         saveAsPdfButton.style.color = shareButtonSpanStyles.color;
 
+        // Manually set some styles that aren't copied for some reason
+        saveAsPdfButton.style.paddingLeft = "10px"
+        saveAsPdfButton.style.paddingRight = "10px"
+
+        // Set hover state styles
         saveAsPdfButton.addEventListener("mouseover", () => {
           saveAsPdfButton.style.backgroundColor = "";
           saveAsPdfButton.style.color = "";
@@ -58,23 +79,23 @@ function addSaveAsPdfButton(shadowHost) {
           saveAsPdfButton.style.color = shareButtonSpanStyles.color;
 
         }); 
-
-        saveAsPdfButton.addEventListener("click", () => {
-          const parentComment = shadowHost.parentElement;
-          if (parentComment) {
-            var commentContent = parentComment.querySelector(COMMENT_CONTENT_SELECTOR);
-          }
-          handlePdfClick(commentContent);
-        });
   
         // Insert "Save as PDF" to the right of "Share"
         shareButton.parentNode.insertBefore(
           saveAsPdfButton,
           shareButton.nextSibling.nextSibling
         );
+
+        // On click, retrieve text of the clicked comment
+        saveAsPdfButton.addEventListener("click", () => {
+          const parentComment = rootComment.parentElement;
+          if (parentComment) {
+            var commentContent = parentComment.querySelector(COMMENT_TEXT);
+          }
+          handlePdfClick(commentContent);
+        });
       }
     }
-    
   }
 }
 
@@ -96,21 +117,20 @@ const handlePdfClick = async (e) => {
 
   const pdfContent = document.createElement('div');
   pdfContent.innerHTML = pdfStyles + e.innerHTML;
-
   html2pdf().set(html2pdfOptions).from(pdfContent).save();
 }
 
-// Finds all comment elements and adds the Save as PDF button to each
-function observeShareButtons() {
-  const commentElements = document.querySelectorAll(COMMENT_SELECTOR);
-  commentElements.forEach((commentElement) => {
-
-    if (commentElement.shadowRoot) {
-      console.log(`Shadow DOM for comment '${commentElement}' has loaded.`);
-      addSaveAsPdfButton(commentElement);
+// Finds all comments and triggers adding the Save as PDF button to each
+function getAllComments() {
+  const commentNodes = document.querySelectorAll(COMMENT_ROW);
+  commentNodes.forEach((commentNode) => {
+    // Wait for nested shadow DOM to load
+    if (commentNode.shadowRoot) {
+      console.log(`Shadow DOM for comment '${commentNode}' has loaded.`);
+      addSaveAsPdfButton(commentNode);
     }
   });
 }
 
-observeShareButtons();
-setInterval(observeShareButtons, 3000); // every 3s
+getAllComments();
+setInterval(getAllComments, 3000); // every 3s
--- content2.js
+++ content2.js
@@ -1,13 +1,16 @@
 const COMMENT_TEST_ID = 'div[data-testid="comment"]';
-const BUTTON_XPATH = `//button[contains(text(), 'Share')]`;
+const SAVE_AS_PDF_BUTTON = 'button#saveAsPdfButton';
+const SHARE_BUTTON_XPATH = `//button[contains(text(), 'Share')]`;
 
 function addSaveAsPdfButton(shareButton) {
   const grandparentElement = shareButton.parentElement.parentElement;
-
   if (grandparentElement) {
-    const saveAsPDFButton = grandparentElement.querySelector('button#saveAsPdfButton');
+    const saveAsPDFButton = grandparentElement.querySelector(SAVE_AS_PDF_BUTTON);
+
+    // If Save as PDF button doesn't already exist
     if (!saveAsPDFButton) {
 
+      // Clone Share button and update text
       const saveAsPdfButton = shareButton.cloneNode(true);
       saveAsPdfButton.textContent = "Save as PDF";
       saveAsPdfButton.id = "saveAsPdfButton";
@@ -16,6 +19,7 @@ function addSaveAsPdfButton(shareButton) {
       const grandParentElement = parentElement.parentElement;
       const greatGreatGrandParentElement = grandParentElement.parentElement.parentElement;
 
+      // Insert "Save as PDF" to the right of "Share"
       if (grandParentElement) {
         grandParentElement.insertBefore(
           saveAsPdfButton,
@@ -51,13 +55,13 @@ const handlePdfClick = async (e) => {
 
   const pdfContent = document.createElement('div');
   pdfContent.innerHTML = pdfStyles + e.innerHTML;
-
   html2pdf().set(html2pdfOptions).from(pdfContent).save();
 }
 
-function observeShareButtons() {
+// Finds all Share buttons and triggers adding the Save as PDF button to each
+function getAllShareButtons() {
   const shareButtons = document.evaluate(
-    BUTTON_XPATH,
+    SHARE_BUTTON_XPATH,
     document,
     null,
     XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE,
@@ -70,5 +74,5 @@ function observeShareButtons() {
   }
 }
 
-observeShareButtons();
-setInterval(observeShareButtons, 3000); // every 3s
+getAllShareButtons();
+setInterval(getAllShareButtons, 3000); // every 3s
